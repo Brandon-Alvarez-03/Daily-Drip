690270b23a7fc97cc23724d97feabd876ec6f2d4
Danny Yu <tiramisu@Dannys-MBP.home>
2022-12-17 16:03:23 -0500

    change interger field, clean up router and settings, add media folder

--- server/dailydrip/models.py
+++ server/dailydrip/models.py
@@ -5,7 +5,7 @@ class Day(models.Model):
   owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='days', on_delete=models.CASCADE, null=True)
   description = models.CharField(max_length=512)
   category = models.CharField(max_length=64, default="other")
-  usage = models.IntegerField(default='0')
+  usage = models.IntegerField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   
