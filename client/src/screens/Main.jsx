0e89db28ecb7b690e332769fec229580339cc85c
Danny Yu <tiramisu@Dannys-MBP.home>
2022-12-17 22:30:04 -0500

    protect dashboard

--- client/src/screens/Main.jsx
+++ client/src/screens/Main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
-import Profile from "./Profile.jsx";
+import { Routes, Route } from "react-router-dom";
+import DashBoard from "./DashBoard.jsx";
 import Login from "./Login.jsx";
 import Signup from "./Signup.jsx";
 import Tracker from "./Tracker.jsx";
 import Newsletter from "./Newsletter.jsx";
+import ProtectedRoute from '../services/protectedRoute'
 
 function Main() {
   return (
     <div className="main">
       <Routes>
-        <Route path="/" element={<Profile />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<DashBoard />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/tracker" element={<Tracker />} />
