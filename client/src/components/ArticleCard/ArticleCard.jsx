6a6c898d30fdbd32b71d36619b4a7e19dd4d0763
Victor Apaez <vic.apaez@gmail.com>
2022-12-18 00:16:33 -0500

    created article page

---
+++ client/src/components/ArticleCard/ArticleCard.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import "./ArticleCard.css";
+
+function ArticleCard({ data }) {
+  return (
+    <div className="card-container">
+      <div className="card-img" src="" alt="" />
+      <div className="card-info">
+        <h3 className="card-title">{data.title}</h3>
+        <p className="card-description">{data.description}</p>
+        <div className="card-icons-container">
+          <div className="card-icon">
+            <p>{data.author}</p>
+          </div>
+          <div className="card-icon">
+            <p>{data.date}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ArticleCard;
--- client/src/screens/Newsletter.jsx
+++ client/src/screens/Newsletter.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import ArticleCard from "../components/ArticleCard/ArticleCard";
+
+const articles = [
+  {
+    title: "The Benefits of Exercise",
+    description:
+      "Regular exercise has numerous benefits for both physical and mental health. It can help improve cardiovascular health, reduce the risk of chronic diseases such as obesity and type 2 diabetes, and boost mood and mental clarity. In addition, regular exercise can help to improve sleep, reduce stress and anxiety, and enhance overall quality of life.",
+    author: "Jane Doe",
+    date: "2022-12-18",
+  },
+];
 
 function Newsletter() {
   return (
-    <div>Newsletter</div>
-  )
+    <div>
+      {articles.map((article, i) => {
+        return <ArticleCard key={i} data={article}></ArticleCard>;
+      })}
+    </div>
+  );
 }
 
-export default Newsletter
+export default Newsletter;
