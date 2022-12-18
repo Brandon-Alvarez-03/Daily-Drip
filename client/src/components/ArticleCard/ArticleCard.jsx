ed7a53999c1f56f9b846c04005aa8bb23f192fe4
Victor Apaez <vic.apaez@gmail.com>
2022-12-18 02:01:15 -0500

    fixed heading margin

--- client/src/components/ArticleCard/ArticleCard.jsx
+++ client/src/components/ArticleCard/ArticleCard.jsx
@@ -4,24 +4,27 @@ import "./ArticleCard.css";
 function ArticleCard({ data }) {
   return (
     <div className="card-container">
-      <div
-        className="card-img"
-        style={{
-          backgroundImage: `url(${data.imgUrl})`,
-        }}
-        alt=""
-      />
-      <div className="card-info">
-        <h3 className="card-title">{data.title}</h3>
-        <p className="card-description">{data.description}</p>
-        <span>continue reading...</span>
-        <div className="card-icons-container">
-          <div className="card-icon">
-            <p>{data.author}</p>
-          </div>
-          <div className="card-icon">
-            <p>{data.date}</p>
-          </div>
+      <div>
+        <div
+          className="card-img"
+          style={{
+            backgroundImage: `url(${data.imgUrl})`,
+          }}
+          alt=""
+        />
+        <div className="card-info">
+          <h3 className="card-title">{data.title}</h3>
+          <p className="card-description">{data.description}</p>
+          <span>continue reading...</span>
+        </div>
+      </div>
+
+      <div className="card-icons-container">
+        <div className="card-icon">
+          <p>{data.author}</p>
+        </div>
+        <div className="card-icon">
+          <p>{data.date}</p>
         </div>
       </div>
     </div>
