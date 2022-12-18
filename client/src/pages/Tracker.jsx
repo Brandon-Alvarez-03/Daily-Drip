f8fda13f5b385f89929fc807e30f3ff940fc047e
Brandon-Alvarez-03 <100237632+Brandon-Alvarez-03@users.noreply.github.com>
2022-12-17 22:31:54 -0500

    start water tracker

--- client/src/pages/Tracker.jsx
+++ client/src/pages/Tracker.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Tracker() {
+  const waterUsages = [
+    {gallons: 10},
+    {gallons: 20},
+    {gallons: 30},
+    {gallons: 40},
+  ];
+
+  // Calculate total gallons of water used
+  const totalGallons = waterUsages.reduce(
+    (acc, usage) => acc + usage.gallons,
+    0
+  );
+
+  // Convert gallons to liters
+  const totalLiters = totalGallons * 3.78541;
+
   return (
-    <div>Tracker</div>
-  )
+    <div>
+      <h2>Water Usage Calculator</h2>
+      <p>Total gallons of water used: {totalGallons}</p>
+      <p>Total liters of water used: {totalLiters}</p>
+    </div>
+  );
 }
 
-export default Tracker
+export default Tracker;
